b8a1945ab5d57cf428e8cf113e3327b869d317c2
Tsuno Yuya <2ch.yuya@gmail.com>
2020-05-15 17:11:34 +0900

    ゲーム投稿機能追加

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,54 @@
 class GamesController < ApplicationController
+  before_action :set_game, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @games = Game.all
+  end
+  
+  def new
+    @game = Game.new
+  end
+  
+  def create
+    @game = Game.new(game_params)
+    if params[:back]
+      render :new
+    else
+      if @game.save
+        redirect_to game_path(@game.id), notice: "ゲームを作成しました！"
+      else
+        render :new
+      end
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @game.update(game_params)
+      redirect_to game_path(@game.id), notice: "ゲームを編集しました！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @game.destroy
+    redirect_to games_path, notice:"ゲームを削除しました！"
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:title, :company, :genre, :price, :released_at, :story, :icon, :icon_cache)
+  end
+
+  def set_game
+    @game = Game.find(params[:id])
+  end
+
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,19 @@
 class Game < ApplicationRecord
+  # has_many :reviews, dependent: :destroy
+  # has_many :comments, dependent: :destroy
+  # has_many :having_games, dependent: :destroy
+  # has_many :likes, dependent: :destroy
+
+  # has_many :review_categories, through: :reviews
+  # has_many :review_scores, through: :reviews
+
+  # has_many :users, through: :having_games
+  # has_many :users, through: :likes
+  # has_many :users, through: :comments
+
+  enum genre: { action: 0, action_RPG: 1, RPG: 2, adventure: 3, action_adventure: 4, action_shouting: 5,
+  shouting: 6, FPS_TPS: 7, pazzle: 8, simulation: 9, simulation_RPG: 10, 
+  racing_action: 11, race: 12, music: 13, battle: 14, sports: 15, others: 16 }
+  
+  mount_uploader :icon, IconUploader
 end
