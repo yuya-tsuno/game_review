6590e06f761eeaff457049d88782d2fefd8f64d2
Tsuno Yuya <2ch.yuya@gmail.com>
2020-05-15 18:17:59 +0900

    reviewのアソシエーションとCRUD機能追加

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,4 @@
 class Review < ApplicationRecord
-  # belongs_to :game, foreign_key: :game_id
-  # belongs_to :user, foreign_key: :user_id
+  belongs_to :game, foreign_key: :game_id
+  belongs_to :user, foreign_key: :user_id
 end
