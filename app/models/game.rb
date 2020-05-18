272483a2eef3eec247569aff28b0b46a71bca1d4
Tsuno Yuya <2ch.yuya@gmail.com>
2020-05-18 15:49:50 +0900

    点数によるsort機能実装（エラー）

--- app/models/game.rb
+++ app/models/game.rb
@@ -8,10 +8,12 @@ class Game < ApplicationRecord
   has_many :owns, dependent: :destroy
   has_many :own_users, through: :owns, source: :user
 
-  enum genre: { action: 1, action_RPG: 2, RPG: 3, adventure: 4, action_adventure: 5,
-                shouting: 6, FPS_TPS: 7, pazzle: 8, simulation: 9, racing: 10, 
-                music: 11, battle: 12, sports: 13, others: 14 
+  enum genre: { action: 1, action_RPG: 2, RPG: 3, adventure: 4, action_adventure: 5, shouting: 6, FPS_TPS: 7, 
+                pazzle: 8, simulation: 9, racing: 10, music: 11, battle: 12, sports: 13, other: 14 
               }
   
-  # ここに自前のスコープを設定？
+  ransacker :direction_score do
+    query = '(SELECT AVG(reviews.direction) FROM reviews WHERE reviews.game_id = game.id GROUP BY reviews.game_id)'
+    Arel.sql(query)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 n = 0
-16.times do |n|
-  n = n + 1
+14.times do |n|
+  n = n + 1 
   Game.create(
     title: "title#{n}", 
     company: "company#{n}", 
@@ -45,60 +45,30 @@ n = 0
     )
 end
 
-user = User.new(
+guest_user = User.new(
   name: "Guest", 
   email: "guest@example.com", 
   password: "pass_for_guest",
   introduce: "Thank you for your watching my review app, Guest!",
   admin: false
   )
-user.skip_confirmation!
-user.save
+guest_user.skip_confirmation!
+guest_user.save
 
-user = User.new(
+admin_user = User.new(
   name: "Admin", 
   email: "admin@example.com", 
   password: "pass_for_admin",
   introduce: "Thank you for your watching my review app, Admin!",
   admin: true
   )
-user.skip_confirmation!
-user.save
+admin_user.skip_confirmation!
+admin_user.save
 
-16.times do |n|
-  n = n + 1
-  Game.create(
-    title: "title#{n}", 
-    company: "company#{n}", 
-    price: n*1000,
-    genre: n,
-    released_at: "2020-05-#{n}"
-    )
-
-  Review.create(
-    direction: n,
-    operability: n,
-    story: n,
-    volume: n,
-    like: n,
-    body: "コメント#{n}",
-    game_id: n,
-    user_id: n
-    )
-
-    Comment.create(
-      body: "commment_body#{n}",
-      game_id: n+1,
-      user_id: n+1
-      )
-
-    Favorite.create(
-      game_id: n+2,
-      user_id: n+2
-    )
-
-    Own.create(
-      game_id: n+3,
-      user_id: n+3
-    )
-end
+Game.create(
+  title: "non_review_title", 
+  company: "non_named_company", 
+  price: 1,
+  genre: 4,
+  released_at: "2020-04-04"
+  )
