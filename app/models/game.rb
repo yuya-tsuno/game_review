5187179a743629aba50e503f91aeded80db0b203
Tsuno Yuya <2ch.yuya@gmail.com>
2020-05-18 12:19:25 +0900

    chckboxの重複問題、応急処置

--- app/models/game.rb
+++ app/models/game.rb
@@ -8,9 +8,10 @@ class Game < ApplicationRecord
   has_many :owns, dependent: :destroy
   has_many :own_users, through: :owns, source: :user
 
-  enum genre: { action: 0, action_RPG: 1, RPG: 2, adventure: 3, action_adventure: 4, action_shouting: 5,
-  shouting: 6, FPS_TPS: 7, pazzle: 8, simulation: 9, simulation_RPG: 10, 
-  racing_action: 11, race: 12, music: 13, battle: 14, sports: 15, others: 16 }
+  enum genre: { action: 1, action_RPG: 2, RPG: 3, adventure: 4, action_adventure: 5,
+                shouting: 6, FPS_TPS: 7, pazzle: 8, simulation: 9, racing: 10, 
+                music: 11, battle: 12, sports: 13, others: 14 
+              }
   
   # ここに自前のスコープを設定？
 end
