8d150958ec3e6cc524656d589e62b1b17ff85a73
Tsuno Yuya <2ch.yuya@gmail.com>
2020-05-20 18:36:40 +0900

    画像データの削除

--- app/uploaders/icon_uploader.rb
+++ app/uploaders/icon_uploader.rb
@@ -8,4 +8,11 @@ class IconUploader < CarrierWave::Uploader::Base
   version :resize_for_user_icon do 
     process resize_to_limit: [100, 100]
   end
+
+  storage :file # 保存形式の設定。他に使うものとしてはfog形式などがある。
+
+  # 画像ファイルの保存先の設定。保存先を指定するには `store_dir` というメソッドに定義します。
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
 end
