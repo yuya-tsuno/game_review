34fcbff26ea7b3f08d46fd63e23287bf24468c73
Tsuno Yuya <2ch.yuya@gmail.com>
2020-09-25 17:52:27 +0900

    adminによる非同期通信コメント変更 #7

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,6 @@ Rails.application.routes.draw do
   get 'relationships/create'
   get 'relationships/destroy'
 
-  namespace :admin do
-    root 'games#index'
-    resources :games do
-      resources :comments, only: [:create, :destroy]
-    end
-    resources :comments, only: [:index, :edit, :update, :destroy]
-    resources :reviews
-    resources :owns, only: [:index, :create, :destroy]
-    resources :favorites, only: [:index, :create, :destroy]
-    resources :users
-    resources :relationships, only: [:index, :create, :destroy]
-  end
-  
   post '/users/sign_in_as_a_guest', to: 'users#guest'
   post '/users/sign_in_as_an_admin', to: 'users#admin'
   
@@ -29,12 +16,11 @@ Rails.application.routes.draw do
     get 'login', to: 'users/sessions#new'
     get 'logout', to: 'users/sessions#destroy'
   end
-
+  
   root 'games#index'
   resources :games, only: [:index, :new, :create, :show] do
     resources :comments, only: [:create, :destroy]
   end
-
   resources :comments, only: [:edit, :update]
   resources :reviews, only: [:new, :create, :show, :edit, :update, :destroy]
   resources :owns, only: [:create, :destroy]
@@ -44,7 +30,19 @@ Rails.application.routes.draw do
   resources :conversations do
     resources :messages
   end
-
+  
+  namespace :admin do
+    root 'games#index'
+    resources :games do
+      resources :comments, only: [:create, :destroy]
+    end
+    resources :comments, only: [:index, :edit, :update, :destroy]
+    resources :reviews
+    resources :owns, only: [:index, :create, :destroy]
+    resources :favorites, only: [:index, :create, :destroy]
+    resources :users
+    resources :relationships, only: [:index, :create, :destroy]
+  end
   
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   
